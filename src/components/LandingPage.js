f277da48e3c7b3a46a630e2bfd63a85716fa297c
lapohjan <59822237+lapohjan@users.noreply.github.com>
2020-09-13 18:44:14 +0300

    modify font awesome symbols and add tabs for portfolio page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -8,18 +8,35 @@ export default function LandingPage() {
             <Grid className='landing-grid'>
                 <Cell col={12}>
                     <img
-                    src="https://source.unsplash.com/xw69fz33sKg"
-                    alt="lake view at night"
-                    className="lake" style={{height:'250px', paddingTop:'5em'}}
+                    // src="https://source.unsplash.com/xw69fz33sKg"
+                    // alt="lake view at night"
+                    src="../../laura.jpg"
+                    alt="profile img"
+                    className="profile_pic" 
+                    style={{height:'200px', borderRadius: '50%', marginTop:'5em'}}
                     />
                     <div className="banner-text">
                         <h1>Laura Pohjanpalo</h1>
 
                         <hr/>
 
-                        <p>HTML/CSS | JavaScript | React | Node | Express | MongoDB </p>
+                        <p>HTML/CSS | JavaScript | React | Node </p>
+                            {/* | Express | Firebase | MongoDB </p> */}
                     <div className="social-links">
-                        
+                    
+                    {/* LinkedIn */}
+                    <a href="https://www.linkedin.com/in/laura-pohjanpalo/" rel="noopener noreferrer" target="_blank">
+                        <i class="fa fa-linkedin-square"  aria-hidden="true" />
+                    </a>
+                     {/* GitHub */}
+                     <a href="https://github.com/lapohjan?tab=repositories" rel="noopener noreferrer" target="_blank">
+                        <i class="fa fa-github-square"  aria-hidden="true" />
+                    </a>
+                    {/* Portfolio */}
+                    <a href="https://lauratellervo.fi/" rel="noopener noreferrer" target="_blank">
+                        {/* <i class="fas fa-desktop"  aria-hidden="true" /> */}
+                        <i class="fa fa-home" aria-hidden="true" />
+                    </a>
                     </div>
                     </div>
                 </Cell>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Tabs, Tab } from 'react-mdl'
 
-export default function Projects() {
+function Projects() {
+
+    const [active, setActive] = useState(0);
+
+    const handleClick = e => {
+        const index = parseInt(e.target.id, 0);
+        if (index !== active) {
+          setActive(index);
+        }
+      };
+
     return (
-        <div>
-            <h1>Projects Page</h1>
+        <div className="category-tabs">
+            {/* <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple></Tabs> */}
+            <Tabs>
+                <Tab onClick={handleClick} active={active === 0} id={0}>
+                    React
+                </Tab>
+                <Tab onClick={handleClick} active={active === 1} id={1}>
+                    Node
+                </Tab>
+                <Tab onClick={handleClick} active={active === 2} id={2}>
+                    HTML/CSS
+                </Tab>
+            </Tabs>
+            {/* <>
+        <Content active={active === 0}>
+          <h1>React</h1>
+        </Content>
+        <Content active={active === 1}>
+          <h1>Node</h1>
+        </Content>
+        <Content active={active === 2}>
+          <h1>HTML/CSS</h1>
+        </Content>
+      </> */}
+            <section className="projects-gird"></section>
         </div>
-    )
+    );
 }
+
+export default Projects;
