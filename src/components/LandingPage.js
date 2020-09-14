8b60010398fa13d14e1ee8c3679dbc265bac3dc7
lapohjan <59822237+lapohjan@users.noreply.github.com>
2020-09-14 13:18:08 +0300

    add responsiveness to landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -19,27 +19,20 @@ export default function LandingPage() {
                     />
                     <div className="banner-text">
                         <h1>Laura Pohjanpalo</h1>
-
                         <hr/>
-
                         <p>HTML/CSS | JavaScript | React | Node </p>
-                            {/* | Express | Firebase | MongoDB </p> */}
-                    <div className="social-links">
-                    
-                    {/* LinkedIn */}
-                    <a href="https://www.linkedin.com/in/laura-pohjanpalo/" rel="noopener noreferrer" target="_blank">
-                        <i class="fa fa-linkedin-square"  aria-hidden="true" />
-                    </a>
-                     {/* GitHub */}
-                     <a href="https://github.com/lapohjan?tab=repositories" rel="noopener noreferrer" target="_blank">
-                        <i class="fa fa-github-square"  aria-hidden="true" />
-                    </a>
-                    {/* Portfolio */}
-                    <a href="https://lauratellervo.fi/" rel="noopener noreferrer" target="_blank">
-                        {/* <i class="fas fa-desktop"  aria-hidden="true" /> */}
-                        <i class="fa fa-home" aria-hidden="true" />
-                    </a>
-                    </div>
+                            <div className="social-links">
+                                <a href="https://www.linkedin.com/in/laura-pohjanpalo/" rel="noopener noreferrer" target="_blank">
+                                    <i class="fa fa-linkedin-square"  aria-hidden="true" />
+                                </a>
+                                <a href="https://github.com/lapohjan?tab=repositories" rel="noopener noreferrer" target="_blank">
+                                    <i class="fa fa-github-square"  aria-hidden="true" />
+                                </a>
+                                <a href="https://lauratellervo.fi/" rel="noopener noreferrer" target="_blank">
+                                    {/* <i class="fas fa-desktop"  aria-hidden="true" /> */}
+                                    <i class="fa fa-home" aria-hidden="true" />
+                                </a>
+                            </div>
                     </div>
                 </Cell>
             </Grid>
