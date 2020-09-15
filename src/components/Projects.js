2602bceaac449dc185b1536361af406979de719a
lapohjan <59822237+lapohjan@users.noreply.github.com>
2020-09-15 14:28:35 +0300

    added content and links to project cards

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,18 +10,23 @@ class Projects extends Component {
 
   toggleCategories() {
 
+  {/* React projects in tab 1 */}
     if(this.state.activeTab === 0){
       return(
         <div className="projects-grid">
           {/* Project 1 */}
           <Card shadow={5} style={{minWidth: '300px', margin: '2rem', borderRadius: '10px'}}>
-            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >React Project #1</CardTitle>
+            <CardTitle style={{color: '#fff', height: '220px', background: 'url(./music.jpg) top, center / cover'}} >Music From My Year</CardTitle>
             <CardText>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+            A React App that uses Spotify API. Summer Project 2020, Helsinki Business College. Team: Laura Pohjanpalo, Elisabet Laitinen, Katariina Vuorinen.
             </CardText>
             <CardActions border>
+            <a href="https://github.com/lapohjan/music-app/tree/dev-branch" target="__blank">
               <Button colored>GitHub</Button>
+            </a>
+            <a href="https://lauratellervo.fi/mymusic/" target="__blank">
               <Button colored>Live Demo</Button>
+              </a>
             </CardActions>
             <CardMenu style={{color: '#fff'}}>
             </CardMenu>
@@ -29,13 +34,17 @@ class Projects extends Component {
 
           {/* Project 2 */}
           <Card shadow={5} style={{minWidth: '300px', margin: '2rem', borderRadius: '10px'}}>
-            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >React Project #2</CardTitle>
+            <CardTitle style={{color: '#000000', height: '220px', background: 'url(./socialnetwork.jpg) center / cover'}} >Social Network</CardTitle>
             <CardText>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+              A social network app for messaging. Using React with Redux, Firebase and Materialize. Key features: Firebase Authentication, Firebase database, web page for showing messages and notifications.
             </CardText>
             <CardActions border>
+            <a href="https://github.com/lapohjan/social-network" target="__blank">
               <Button colored>GitHub</Button>
+              </a>
+              <a href="https://network-3c607.web.app/" target="__blank">
               <Button colored>Live Demo</Button>
+              </a>
             </CardActions>
             <CardMenu style={{color: '#fff'}}>
             </CardMenu>
@@ -43,36 +52,124 @@ class Projects extends Component {
 
           {/* Project 3 */}
           <Card shadow={5} style={{minWidth: '300px', maxWidth: '500px', margin: '2rem', borderRadius: '10px'}}>
-            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >React Project #3</CardTitle>
+            <CardTitle style={{color: '#fff', height: '220px', background: 'url(./speedgame.jpg) center / cover'}} >Speed Game</CardTitle>
             <CardText>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+              A speed game done with React. Catch the hamster as the speed goes up. Circle backgrounds are changing color when the hamster appers. Includes a custom cursor and some sound effects.
             </CardText>
             <CardActions border>
+            <a href="https://github.com/lapohjan/reactjs_sandbox/tree/master/5_task" target="__blank">
               <Button colored>GitHub</Button>
+              </a>
+              <a href="https://www.lauratellervo.fi/speedgame/" target="__blank">
               <Button colored>Live Demo</Button>
+              </a>
             </CardActions>
             <CardMenu style={{color: '#fff'}}>
             </CardMenu>
           </Card>
         </div>
+      )
 
-
-      )
-    } else if(this.state.activeTab === 1) {
-      return (
-        <div><h1>This is React</h1></div>
-      )
-    } else if(this.state.activeTab === 2) {
-      return (
-        <div><h1>This is Node</h1></div>
-      )
-    } else if(this.state.activeTab === 3) {
-      return (
-        <div><h1>This is HTML/CSS</h1></div>
-      )
-    }
-
+  {/* Node projects in tab 2 */}
   }
+  else if(this.state.activeTab === 1){
+    return(
+      <div className="projects-grid">
+        {/* Project 1 */}
+        <Card shadow={5} style={{minWidth: '300px', margin: '2rem', borderRadius: '10px'}}>
+          <CardTitle style={{color: '#000000', height: '220px', background: 'url(./weather.jpg) center / cover'}} >Weather App</CardTitle>
+          <CardText>
+          A Node weather app that two different APIs from Mapbox and Weatherstack. Working application deployed to Heroku. A project that I did following The Complete Node.js Developer Course from Udemy.
+          </CardText>
+          <CardActions border>
+          <a href="https://github.com/lapohjan/node-weather-website" target="__blank">
+            <Button colored>GitHub</Button>
+          </a>
+          <a href="https://northern-light-weather.herokuapp.com/" target="__blank">
+            <Button colored>Live Demo</Button>
+            </a>
+          </CardActions>
+          <CardMenu style={{color: '#fff'}}>
+          </CardMenu>
+        </Card>
+
+        {/* Project 2 */}
+        <Card shadow={5} style={{minWidth: '300px', margin: '2rem', borderRadius: '10px'}}>
+          <CardTitle style={{color: '#000000', height: '220px', background: 'url(./icecreams.jpg) top / cover'}} >Ice-creams and Dreams</CardTitle>
+          <CardText>
+            An ice-cream app. Choose your flavor and get a nice picture of an ice-cream cone with the price. A Node database exercise. Ice-creams drawn by me.
+          </CardText>
+          <CardActions border>
+          <a href="https://github.com/lapohjan/ice-cream-app" target="__blank">
+            <Button colored>GitHub</Button>
+            </a>
+            <a href="https://northern-light-ice-cream-app.herokuapp.com/" target="__blank">
+            <Button colored>Live Demo</Button>
+            </a>
+          </CardActions>
+          <CardMenu style={{color: '#fff'}}>
+          </CardMenu>
+        </Card>
+
+        {/* Project 3
+        <Card shadow={5} style={{minWidth: '300px', maxWidth: '500px', margin: '2rem', borderRadius: '10px'}}>
+          <CardTitle style={{color: '#fff', height: '220px', background: 'url(./xxx.jpg) center / cover'}} >Speed Game</CardTitle>
+          <CardText>
+            An app...
+          </CardText>
+          <CardActions border>
+          <a href="https://github.com/">
+            <Button colored>GitHub</Button>
+            </a>
+            <a href="https://www.lauratellervo.fi/">
+            <Button colored>Live Demo</Button>
+            </a>
+          </CardActions>
+          <CardMenu style={{color: '#fff'}}>
+          </CardMenu>
+        </Card> */}
+      </div>
+    )
+
+    }
+    else if(this.state.activeTab === 2){
+      return(
+        <div className="projects-grid">
+          {/* Project 1 */}
+          <Card shadow={5} style={{minWidth: '300px', margin: '2rem', borderRadius: '10px'}}>
+            <CardTitle style={{color: '#000000', height: '220px', background: 'url(./population.jpg) center / cover'}} >Population Graph</CardTitle>
+            <CardText>
+            A population graph app done at Mimmitkoodaa MOOC by Sympa. Using HTML, CSS, Javascript and JSON data from a World Bank API. 
+            </CardText>
+            <CardActions border>
+            <a href="https://github.com/lapohjan/Mimmit_koodaa" target="__blank">
+              <Button colored>GitHub</Button>
+            </a>
+            <a href="https://lauratellervo.fi/population/" target="__blank">
+              <Button colored>Live Demo</Button>
+              </a>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+            </CardMenu>
+          </Card>
+        </div>
+      )
+    
+  } else if(this.state.activeTab === 0) {
+    return (
+      <div><h1>This is React</h1></div>
+    )
+  } else if(this.state.activeTab === 1) {
+    return (
+      <div><h1>This is Node</h1></div>
+    )
+  } else if(this.state.activeTab === 2) {
+    return (
+      <div><h1>This is HTML/CSS</h1></div>
+    )
+  }
+
+}
 
 
 
