224d14fe1ea669f4182b5e22c0c3682a34bddd58
lapohjan <59822237+lapohjan@users.noreply.github.com>
2020-10-20 13:44:02 +0300

    add new project to projects

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
     <Layout>
         <Header className="header-color" title={<Link style={{textDecoration:'none', color:'white'}} to="/">My Portfolio</Link>} scroll>
             <Navigation>
-                {/* <Link to="/" className="toplink">Home</Link> */}
                 <Link to="/resume" className="toplink">Resume</Link>
                 {/* <Link to="/aboutme" className="toplink">About Me</Link> */}
                 <Link to="/projects" className="toplink">Projects</Link>
@@ -19,7 +18,6 @@ function App() {
         </Header>
         <Drawer title={<Link style={{textDecoration:'none', color:'black'}} to="/">My Portfolio</Link>}>
             <Navigation>
-            {/* <Link to="/" className="toplink">Home</Link> */}
             <Link to="/resume">Resume</Link>
             {/* <Link to="/aboutme">About Me</Link> */}
             <Link to="/projects">Projects</Link>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -15,15 +15,15 @@ class Projects extends Component {
         <div className="projects-grid">
           {/* Project 1 */}
           <Card shadow={5} style={{minWidth: '300px', margin: '2rem', borderRadius: '10px'}}>
-            <CardTitle style={{color: '#fff', height: '220px', background: 'url(./music.jpg) top, center / cover'}} >Music From My Year</CardTitle>
+            <CardTitle style={{color: '#fff', textShadow:'1px 1px #000000', height: '220px', background: 'url(./music.jpg) top, center / cover'}} >Music From My Year</CardTitle>
             <CardText>
             A React App that uses Spotify API. Summer Project 2020, Helsinki Business College. Team: Laura Pohjanpalo, Elisabet Laitinen, Katariina Vuorinen.
             </CardText>
             <CardActions border>
-            <a href="https://github.com/lapohjan/music-app/tree/dev-branch" target="__blank">
+            <a href="https://github.com/lapohjan/music-app/tree/dev-branch" target="__blank" rel="noopener noreferrer">
               <Button colored>GitHub</Button>
             </a>
-            <a href="https://lauratellervo.fi/mymusic/" target="__blank">
+            <a href="https://lauratellervo.fi/mymusic/" target="__blank" rel="noopener noreferrer">
               <Button colored>Live Demo</Button>
               </a>
             </CardActions>
@@ -38,10 +38,10 @@ class Projects extends Component {
               A social network app for messaging. Using React with Redux, Firebase and Materialize. Key features: Firebase Authentication, Firebase database, message board, new post -form, timestamp and notifications.
             </CardText>
             <CardActions border>
-            <a href="https://github.com/lapohjan/social-network" target="__blank">
+            <a href="https://github.com/lapohjan/social-network" target="__blank" rel="noopener noreferrer">
               <Button colored>GitHub</Button>
               </a>
-              <a href="https://network-3c607.web.app/" target="__blank">
+              <a href="https://network-3c607.web.app/" target="__blank" rel="noopener noreferrer">
               <Button colored>Live Demo</Button>
               </a>
             </CardActions>
@@ -51,21 +51,58 @@ class Projects extends Component {
 
           {/* Project 3 */}
           <Card shadow={5} style={{minWidth: '300px', maxWidth: '500px', margin: '2rem', borderRadius: '10px'}}>
-            <CardTitle style={{color: '#fff', height: '220px', background: 'url(./speedgame.jpg) center / cover'}} >Speed Game</CardTitle>
+            <CardTitle style={{color: '#fff', textShadow:'1px 1px #000000', height: '220px', background: 'url(./calendar.jpg) center / cover'}}>Advent calendar</CardTitle>
             <CardText>
-              A speed game done with React. Catch the hamster as the speed goes up. Score is shown when the game is over. Includes a custom cursor. 
+              An advent calendar. Flipping cards, responsive design. A project I did following Thomas Weibenfalk's Youtube tutorial. My version has toy pics behind the doors instead of Christmas jokes.
             </CardText>
             <CardActions border>
-            <a href="https://github.com/lapohjan/reactjs_sandbox/tree/master/5_task" target="__blank">
+            <a href="https://github.com/lapohjan/christmas-calendar" target="__blank" rel="noopener noreferrer">
               <Button colored>GitHub</Button>
               </a>
-              <a href="https://www.lauratellervo.fi/speedgame/" target="__blank">
+              <a href="https://www.lauratellervo.fi/calendar/" target="__blank" rel="noopener noreferrer">
               <Button colored>Live Demo</Button>
               </a>
             </CardActions>
             <CardMenu style={{color: '#fff'}}>
             </CardMenu>
           </Card>
+
+           {/* Project 4 */}
+           <Card shadow={5} style={{minWidth: '300px', maxWidth: '500px', margin: '2rem', borderRadius: '10px'}}>
+            <CardTitle style={{color: '#fff', textShadow:'1px 1px #000000', height: '220px', background: 'url(./nails.jpg) center/cover'}}>Total Recall Nails</CardTitle>
+            <CardText>
+            Color changing nails! This is my own just-for-fun project inspired by a 1990's movie Total Recall. Click on the wristwatch and then any nail you want to "paint". Done with React and RandomColor script. 
+            </CardText>
+            <CardActions border>
+            <a href="https://github.com/lapohjan/total_recall_nails" target="__blank" rel="noopener noreferrer">
+              <Button colored>GitHub</Button>
+              </a>
+              <a href="https://www.lauratellervo.fi/nails/" target="__blank" rel="noopener noreferrer">
+              <Button colored>Live Demo</Button>
+              </a>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+            </CardMenu>
+          </Card>
+
+          {/* Project 5 */}
+          <Card shadow={5} style={{minWidth: '300px', maxWidth: '500px', margin: '2rem', borderRadius: '10px'}}>
+            <CardTitle style={{color: '#fff', height: '220px', background: 'url(./speedgame.jpg) center / cover'}} >Speed Game</CardTitle>
+            <CardText>
+              A speed game done with React. Catch the hamster as the speed goes up. Score is shown when the game is over. Includes a custom cursor. 
+            </CardText>
+            <CardActions border>
+            <a href="https://github.com/lapohjan/reactjs_sandbox/tree/master/5_task" target="__blank" rel="noopener noreferrer">
+              <Button colored>GitHub</Button>
+              </a>
+              <a href="https://www.lauratellervo.fi/speedgame/" target="__blank" rel="noopener noreferrer">
+              <Button colored>Live Demo</Button>
+              </a>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+            </CardMenu>
+          </Card>
+
         </div>
       )
 
@@ -81,10 +118,10 @@ class Projects extends Component {
           A Node weather app that uses two different APIs from Mapbox and Weatherstack. Working application deployed to Heroku. A project that I did following The Complete Node.js Developer Course from Udemy.
           </CardText>
           <CardActions border>
-          <a href="https://github.com/lapohjan/node-weather-website" target="__blank">
+          <a href="https://github.com/lapohjan/node-weather-website" target="__blank" rel="noopener noreferrer">
             <Button colored>GitHub</Button>
           </a>
-          <a href="https://northern-light-weather.herokuapp.com/" target="__blank">
+          <a href="https://northern-light-weather.herokuapp.com/" target="__blank" rel="noopener noreferrer">
             <Button colored>Live Demo</Button>
             </a>
           </CardActions>
@@ -99,10 +136,10 @@ class Projects extends Component {
             An ice-cream app. Choose your flavor and get a nice picture of an ice-cream cone with the price. A Node database exercise. Ice-creams drawn by me.
           </CardText>
           <CardActions border>
-          <a href="https://github.com/lapohjan/ice-cream-app" target="__blank">
+          <a href="https://github.com/lapohjan/ice-cream-app" target="__blank" rel="noopener noreferrer">
             <Button colored>GitHub</Button>
             </a>
-            <a href="https://northern-light-ice-cream-app.herokuapp.com/" target="__blank">
+            <a href="https://northern-light-ice-cream-app.herokuapp.com/" target="__blank" rel="noopener noreferrer">
             <Button colored>Live Demo</Button>
             </a>
           </CardActions>
@@ -141,10 +178,10 @@ class Projects extends Component {
             A population graph app done at Mimmitkoodaa MOOC by Sympa. Using HTML, CSS, Javascript and JSON data from a World Bank API. 
             </CardText>
             <CardActions border>
-            <a href="https://github.com/lapohjan/Mimmit_koodaa" target="__blank">
+            <a href="https://github.com/lapohjan/Mimmit_koodaa" target="__blank" rel="noopener noreferrer">
               <Button colored>GitHub</Button>
             </a>
-            <a href="https://lauratellervo.fi/population/" target="__blank">
+            <a href="https://lauratellervo.fi/population/" target="__blank" rel="noopener noreferrer">
               <Button colored>Live Demo</Button>
               </a>
             </CardActions>
